fd7ee89b77266ebde1dc764ca3971e6a81577754
Roykoboy <orlov.ok1@gmail.com>
2025-07-30 20:49:01 +0500

    upload

---
+++ lab5 Text File Case Converter.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+	FILE* inputFile, * outputFile;
+	char ch;
+
+
+	inputFile = fopen("input.txt", "r");
+	if (inputFile == NULL) {
+		printf("Ошибка открытия файла.\n");
+		return 1;
+	}
+
+
+	outputFile = fopen("output.txt", "w");
+	if (outputFile == NULL) {
+		printf("Ошибка открытия файла.\n");
+		return 1;
+	}
+
+
+	while ((ch = fgetc(inputFile)) != EOF) {
+		ch = toupper(ch);
+		fputc(ch, outputFile);
+		printf("%c", ch);
+	}
+
+
+	fclose(inputFile);
+	fclose(outputFile);
+
+	return 0;
+}
